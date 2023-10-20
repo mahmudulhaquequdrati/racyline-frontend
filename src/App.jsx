ad29f0fcaff8d5937d6db37be9c0e68cdcec66ff
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2023-10-21 02:35:49 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pagers/VetDashboard/Dashboard";
+import Header from "./components/common/Header";
+
+const App = () => {
   return (
-    <div>
-      <h1 className="text-xl">Hello</h1>
-    </div>
+    <main className="">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+      {/* <Footer /> */}
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,14 @@
+import logo from "../../assets/logo.png";
+export default function Header() {
+  return (
+    <div className="px-4 md:px-8 py-6 container mx-auto bg-[#fff] flex justify-between items-center">
+      <div className="">
+        <img src={logo} className="w-[80%] md:w-[100%]" alt="logo" />
+      </div>
+      <div className="flex gap-3 md:gap-8 uppercase">
+        <h2 className="text-sm md:text-base">Sei un medico veterinario? </h2>
+        <h2 className="text-[#E8971F]">accedi</h2>
+      </div>
+    </div>
+  );
+}
