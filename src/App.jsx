6a31dd6920396af748ecfaf926962fd9584de234
Shipon Hossen Raju <msshipon234@gmail.com>
2023-10-31 23:27:28 +0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pagers/VetDashboard/Dashboard";
 import Header from "./components/common/Header";
+import RegistrationAvailabilities from "./pagers/Vets/RegistrationAvailabilities/RegistrationAvailabilities";
 
 const App = () => {
   return (
     <main className="">
       <Header />
+
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/registration-availabilities"
+          element={<RegistrationAvailabilities />}
+        />
       </Routes>
+
       {/* <Footer /> */}
     </main>
   );
