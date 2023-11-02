c75c5759d5e2610ff0d4009792d16261a8b9ee6b
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2023-11-03 02:41:18 +0600

    netlify error updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Appointment from "./pagers/Appointment/Appointment";
 import AppointmentError from "./pagers/Appointment/AppointmentError";
 import AppointmentSuccess from "./pagers/Appointment/AppointmentSuccess";
 import Dashboard from "./pagers/VetDashboard/Dashboard";
-import VetLists from "./pagers/vetLists/vetLists";
+import VetLists from "./pagers/vetLists/VetLists";
 import Login from "./pagers/vetLogin/Login";
 import Registration from "./pagers/vetRegistration/Registration";
 import RegistrationGoogleCalenderConnect from "./pagers/vetRegistration/RegistrationGoogleCalenderConnect";
