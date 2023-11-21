f98c2bee4d12487b1592bf48f8dc585e53687330
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2023-11-21 08:51:55 +0600

    added medical record functionality

--- src/pagers/AnimalClinical/CompleteMedicalRecord.jsx
+++ src/pagers/AnimalClinical/CompleteMedicalRecord.jsx
@@ -9,7 +9,7 @@ const plusIcons = (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_457_2801)">
+    <g clipPath="url(#clip0_457_2801)">
       <path
         d="M16.5111 29.8328C9.14707 29.8328 3.17773 23.8634 3.17773 16.4994C3.17773 9.13543 9.14707 3.16609 16.5111 3.16609C23.8751 3.16609 29.8444 9.13543 29.8444 16.4994C29.8444 23.8634 23.8751 29.8328 16.5111 29.8328ZM15.1777 15.1661H9.8444V17.8328H15.1777V23.1661H17.8444V17.8328H23.1777V15.1661H17.8444V9.83276H15.1777V15.1661Z"
         fill="#339E9F"
@@ -49,7 +49,7 @@ const loadingIcons = (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_452_1894)">
+    <g clipPath="url(#clip0_452_1894)">
       <path
         d="M18.364 5.63609L16.95 7.05009C15.8049 5.90489 14.2982 5.19215 12.6865 5.03333C11.0748 4.87451 9.45794 5.27942 8.11134 6.17908C6.76474 7.07874 5.77174 8.41748 5.30154 9.9672C4.83134 11.5169 4.91302 13.1817 5.53268 14.678C6.15234 16.1742 7.27162 17.4093 8.69983 18.1728C10.128 18.9363 11.7768 19.181 13.3652 18.8652C14.9536 18.5493 16.3833 17.6925 17.4108 16.4407C18.4382 15.1889 18.9999 13.6196 19 12.0001H21C21 14.0823 20.278 16.1001 18.957 17.7096C17.6361 19.3192 15.7979 20.4209 13.7557 20.8271C11.7136 21.2333 9.5937 20.9188 7.75737 19.9373C5.92104 18.9557 4.48187 17.3678 3.68506 15.4441C2.88825 13.5204 2.78311 11.3799 3.38756 9.38739C3.992 7.39486 5.26863 5.67355 6.99992 4.51675C8.73121 3.35996 10.81 2.83925 12.8822 3.04336C14.9544 3.24746 16.8917 4.16375 18.364 5.63609Z"
         fill="black"
@@ -70,11 +70,11 @@ const cancelLoadingIcons = (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_452_1896)">
+    <g clipPath="url(#clip0_452_1896)">
       <path
         d="M12 22C6.477 22 2 17.523 2 12C2 6.477 6.477 2 12 2C17.523 2 22 6.477 22 12C22 17.523 17.523 22 12 22ZM12 10.586L9.172 7.757L7.757 9.172L10.586 12L7.757 14.828L9.172 16.243L12 13.414L14.828 16.243L16.243 14.828L13.414 12L16.243 9.172L14.828 7.757L12 10.586Z"
         fill="black"
-        fill-opacity="0.1"
+        fillOpacity="0.1"
       />
     </g>
     <defs>
@@ -92,11 +92,11 @@ const cancelIcons = (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_452_1902)">
+    <g clipPath="url(#clip0_452_1902)">
       <path
         d="M12 22C6.477 22 2 17.523 2 12C2 6.477 6.477 2 12 2C17.523 2 22 6.477 22 12C22 17.523 17.523 22 12 22ZM12 10.586L9.172 7.757L7.757 9.172L10.586 12L7.757 14.828L9.172 16.243L12 13.414L14.828 16.243L16.243 14.828L13.414 12L16.243 9.172L14.828 7.757L12 10.586Z"
         fill="black"
-        fill-opacity="0.4"
+        fillOpacity="0.4"
       />
     </g>
     <defs>
@@ -121,27 +121,27 @@ const checkIcons = (
   </svg>
 );
 
-const DiaryInitialize = [
-  {
-    _id: "326589734",
-    title: "Vaccinazione antirabbica dynamic",
-    decription: "",
-    date: "20/04/2019",
-    files: ["link-here.pdf", "name_file1.pdf"],
-  },
-  {
-    _id: "3265897334",
-    title: "Vaccinazione antirabbica extra",
-    decription: "",
-    date: "20/04/2019",
-    files: [
-      "link-here.pdf",
-      "name_file1.pdf",
-      "name_file2.pdf",
-      "name_file3.pdf",
-    ],
-  },
-];
+// const DiaryInitialize = [
+//   {
+//     _id: "326589734",
+//     title: "Vaccinazione antirabbica dynamic",
+//     decription: "",
+//     date: "20/04/2019",
+//     report_files: ["link-here.pdf", "name_file1.pdf"],
+//   },
+//   {
+//     _id: "3265897334",
+//     title: "Vaccinazione antirabbica extra",
+//     decription: "",
+//     date: "20/04/2019",
+//     report_files: [
+//       "link-here.pdf",
+//       "name_file1.pdf",
+//       "name_file2.pdf",
+//       "name_file3.pdf",
+//     ],
+//   },
+// ];
 
 function CompleteMedicalRecord() {
   const [isLoading, setIsLoading] = useState();
@@ -153,38 +153,67 @@ function CompleteMedicalRecord() {
     AdditionalNotes: "",
   });
 
-  // final systems
-  const [diaryData, setDiaryData] = useState({
+  const [newNoteOpen, setNewNoteOpen] = useState({
+    isOpen: false,
+    isEditingMode: false,
+    noteIndex: null,
+  });
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState({
     title: "",
     description: "",
     date: "",
+    report_files: [],
   });
-  const [medicalDiary, setMedicalDiary] = useState();
-  const [newNoteOpen, setNewNoteOpen] = useState(false);
-
-  // initialize load data get
-  useEffect(() => {
-    const diaryDataL = JSON.parse(localStorage.getItem("diaryData"));
-    setDiaryData(diaryDataL);
-    console.log("diaryDataL useEffect", diaryDataL);
-    console.log("medicalDiary useEffect", medicalDiary);
-  }, [medicalDiary]);
 
   // add Note handler
   const addNoteHandler = () => {
-    const data = {
-      ...diaryData,
-      files: ["...link-here.pdf"],
-    };
-    // show all medical diary
-    setMedicalDiary([...medicalDiary, data]);
+    if (!newNoteOpen?.isEditingMode && !newNoteOpen?.noteIndex) {
+      setNotes((prevNotes) => {
+        const previusNotes = [...prevNotes];
+        previusNotes.push(note);
+        return previusNotes;
+      });
+    } else if (newNoteOpen?.isEditingMode) {
+      setNotes((prevNotes) => {
+        const previusNotes = [...prevNotes];
+        previusNotes[newNoteOpen?.noteIndex].title = note?.title;
+        previusNotes[newNoteOpen?.noteIndex].description = note?.description;
+        previusNotes[newNoteOpen?.noteIndex].date = note?.date;
+        previusNotes[newNoteOpen?.noteIndex].report_files = [];
+        return previusNotes;
+      });
+    }
+    setNote({
+      title: "",
+      description: "",
+      date: "",
+      report_files: [],
+    });
+    setNewNoteOpen({
+      ...newNoteOpen,
+      isOpen: false,
+    });
+  };
 
-    localStorage.removeItem("diaryData");
-    // set localStorage
-    localStorage.setItem("diaryData", JSON.stringify(medicalDiary));
+  // deleting a note
+  const deleteNoteHandler = (noteId) => {
+    setNotes((prevNotes) => {
+      const previusNotes = [...prevNotes];
+      previusNotes.splice(noteId, 1);
+      return previusNotes;
+    });
+  };
 
-    console.log("data -> ", data);
-    console.log("medicalDiary -> ", medicalDiary);
+  // set info in edit note element
+  const editNoteHandler = (noteId) => {
+    setNewNoteOpen({
+      ...newNoteOpen,
+      isOpen: true,
+      isEditingMode: true,
+      noteIndex: noteId,
+    });
+    setNote(notes[noteId]);
   };
 
   // get Dynamic input value
@@ -205,10 +234,10 @@ function CompleteMedicalRecord() {
     e.preventDefault();
     const data = {
       ...inputData,
-      medicalDiary,
+      notes,
     };
 
-    console.log("data ", data);
+    console.log("data", data);
   };
 
   return (
@@ -224,6 +253,7 @@ function CompleteMedicalRecord() {
         </p>
 
         <form className="flex flex-col gap-y-4" onSubmit={handleSubmit}>
+          {/* Mdical history field start  */}
           <div className="flex flex-col gap-1">
             <label
               htmlFor="medicalHistory"
@@ -239,11 +269,13 @@ function CompleteMedicalRecord() {
               placeholder="Descrivi le caratteristiche, sintomi o fatti di interesse riferiti al tuo animale..."
             ></textarea>
           </div>
+          {/* Mdical history field ends  */}
 
-          {!medicalDiary && (
+          {/* Add note component start */}
+          {!newNoteOpen?.isOpen && (
             <div
               className={`transition duration-300 w-full ${
-                medicalDiary
+                newNoteOpen?.isOpen
                   ? "invisible opacity-0 h-0 "
                   : "visible opacity-1 h-auto mt-10"
               }`}
@@ -254,8 +286,10 @@ function CompleteMedicalRecord() {
               </p>
               <button
                 onClick={() => {
-                  setNewNoteOpen(!newNoteOpen);
-                  setMedicalDiary([]);
+                  setNewNoteOpen({
+                    ...newNoteOpen,
+                    isOpen: !newNoteOpen?.isOpen,
+                  });
                 }}
                 className="flex gap-3 justify-center items-center py-5 border border-dashed mb-4 cursor-pointer rounded-lg w-full"
               >
@@ -266,12 +300,11 @@ function CompleteMedicalRecord() {
               </button>
             </div>
           )}
-
           {/* add medical diary */}
           <div>
             <div
               className={` ${
-                medicalDiary
+                newNoteOpen?.isOpen
                   ? "visible opacity-1 h-auto mt-10 flex flex-col md:flex-row justify-between items-center"
                   : "invisible opacity-0 h-0 "
               } `}
@@ -284,16 +317,23 @@ function CompleteMedicalRecord() {
               </div>
 
               <button
-                onClick={() => setNewNoteOpen(!newNoteOpen)}
+                onClick={() => {
+                  setNewNoteOpen({
+                    ...newNoteOpen,
+                    isOpen: !newNoteOpen?.isOpen,
+                  });
+                }}
                 className="flex gap-3 justify-center items-center py-5 mb-4 cursor-pointer rounded-lg"
               >
                 <span>{plusIcons}</span>
               </button>
             </div>
 
+            {/* Add note component start here  */}
+
             <div
               className={`transition duration-300 ${
-                newNoteOpen
+                newNoteOpen?.isOpen
                   ? "visible opacity-1 h-auto p-4 border border-primary rounded-lg "
                   : "invisible opacity-0 h-0 "
               } `}
@@ -303,15 +343,11 @@ function CompleteMedicalRecord() {
               </p>
 
               <div>
+                {/* Data field  */}
                 <input
                   type="text"
-                  value={inputData.medicalData}
-                  onChange={({ target }) =>
-                    setDiaryData({
-                      ...diaryData,
-                      title: target.value,
-                    })
-                  }
+                  value={note?.title}
+                  onChange={(e) => setNote({ ...note, title: e.target.value })}
                   placeholder="Data *"
                   className={`w-full rounded-lg py-3 px-4 outline-none border-[1px] ${
                     fieldError && inputData?.medicalData === ""
@@ -320,15 +356,11 @@ function CompleteMedicalRecord() {
                   } `}
                 />
 
+                {/* date input  */}
                 <input
                   type="date"
-                  value={inputData.medicalData}
-                  onChange={({ target }) =>
-                    setDiaryData({
-                      ...diaryData,
-                      date: target.value,
-                    })
-                  }
+                  value={note?.date}
+                  onChange={(e) => setNote({ ...note, date: e.target.value })}
                   className={`w-full rounded-lg py-3 px-4 outline-none border-[1px] mt-5 ${
                     fieldError && inputData?.medicalData === ""
                       ? "border-red-500"
@@ -338,11 +370,9 @@ function CompleteMedicalRecord() {
 
                 <textarea
                   className="resize-none w-full mt-5 text-[15px] h-48 outline-none border border-gray-200 rounded-lg"
-                  onChange={({ target }) =>
-                    setDiaryData({
-                      ...diaryData,
-                      description: target.value,
-                    })
+                  value={note?.description}
+                  onChange={(e) =>
+                    setNote({ ...note, description: e.target.value })
                   }
                   placeholder="Inserisci una descrizione..."
                 ></textarea>
@@ -383,6 +413,7 @@ function CompleteMedicalRecord() {
                       />
                     </label>
 
+                    {/* files name  */}
                     <div>
                       <div className="flex flex-col md:flex-row gap-2 items-center justify-between border-b py-4">
                         <div className="flex gap-[10px]">
@@ -436,6 +467,8 @@ function CompleteMedicalRecord() {
                           </svg>
                           <span>Loading...</span>
                         </div>
+                      ) : newNoteOpen?.isEditingMode ? (
+                        "Modifica nota"
                       ) : (
                         "Aggiungi nota"
                       )}
@@ -444,49 +477,54 @@ function CompleteMedicalRecord() {
                 </div>
               </div>
             </div>
+
+            {/* Add note component ends here  */}
           </div>
 
           {/* all notes */}
-          {medicalDiary?.length > 0 && (
+          {notes?.length > 0 && (
             <div>
               <h4 className="font-semibold text-base">Tutte le note</h4>
 
-              {medicalDiary.length &&
-                medicalDiary.map((item, idx) => (
-                  <div key={idx} className="border rounded-lg p-4 mt-4">
-                    <div className="flex flex-col md:flex-row items-center justify-between ">
-                      <h5 className="font-normal text-[15px]">{item?.title}</h5>
-                      <p className="text-[#00000066]"> {item.date} </p>
-                    </div>
-
-                    <div className="py-3 flex gap-1 flex-wrap">
-                      {item.files &&
-                        item.files.map((file, FIdx) => (
-                          <span
-                            key={FIdx}
-                            className="py-1.5 px-3 max-w-max rounded-lg text-[#00000066] bg-[#E5E7EC99]"
-                          >
-                            name_file1.pdf
-                          </span>
-                        ))}
-                    </div>
-
-                    <div className="flex gap-4 mt-2">
-                      <button
-                        className={`w-full rounded-lg py-3 px-4 outline-none text-secondary border-secondary border hover:bg-secondary hover:text-white transition duration-300`}
-                      >
-                        Modifica cartella clinica
-                      </button>
-                      <button
-                        className={`w-full rounded-lg py-3 px-4 outline-none text-secondary border-secondary border hover:bg-secondary hover:text-white transition duration-300`}
-                      >
-                        Elimina
-                      </button>
-                    </div>
+              {notes?.map((item, idx) => (
+                <div key={idx} className="border rounded-lg p-4 mt-4">
+                  <div className="flex flex-col md:flex-row items-center justify-between ">
+                    <h5 className="font-normal text-[15px]">{item?.title}</h5>
+                    <p className="text-[#00000066]"> {item?.date} </p>
                   </div>
-                ))}
+
+                  <div className="py-3 flex gap-1 flex-wrap">
+                    {item?.report_files &&
+                      item?.report_files.map((file, FIdx) => (
+                        <span
+                          key={FIdx}
+                          className="py-1.5 px-3 max-w-max rounded-lg text-[#00000066] bg-[#E5E7EC99]"
+                        >
+                          {file}
+                        </span>
+                      ))}
+                  </div>
+
+                  <div className="flex gap-4 mt-2">
+                    <button
+                      onClick={() => editNoteHandler(idx)}
+                      className={`w-full rounded-lg py-3 px-4 outline-none text-secondary border-secondary border hover:bg-secondary hover:text-white transition duration-300`}
+                    >
+                      Modifica cartella clinica
+                    </button>
+                    <button
+                      onClick={() => deleteNoteHandler(idx)}
+                      className={`w-full rounded-lg py-3 px-4 outline-none text-secondary border-secondary border hover:bg-secondary hover:text-white transition duration-300`}
+                    >
+                      Elimina
+                    </button>
+                  </div>
+                </div>
+              ))}
             </div>
           )}
+
+          {/* Note aggiuntive start here */}
 
           <div className="flex flex-col gap-1 mt-10">
             <label
@@ -504,6 +542,9 @@ function CompleteMedicalRecord() {
             ></textarea>
           </div>
 
+          {/* Note aggiuntive ends here */}
+
+          {/* add your pet start here */}
           <button
             type="submit"
             className={`mt-8 w-full rounded-lg py-3 px-4 outline-none hover:text-secondary border-secondary border bg-secondary hover:bg-transparent text-white transition duration-300`}
@@ -532,6 +573,7 @@ function CompleteMedicalRecord() {
               "Aggiungi il tuo animale"
             )}
           </button>
+          {/* add your pet ends here */}
         </form>
 
         <p className="text-center text-[15px] mt-10 text-[#00000066]">
