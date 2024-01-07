5b76a747542131e57def339c19af9550c9b44036
Kamruzzaman <86624274+kamruz-zzaman@users.noreply.github.com>
2024-01-08 02:24:26 +0600

    hero section update

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import DropDown from "./Dropdown";
 export default function Header() {
@@ -7,7 +7,7 @@ export default function Header() {
   const state = useSelector((state) => state.auth);
 
   const { user, accessToken } = state || {};
-
+  const location = useLocation();
   return (
     <div className="px-4 md:px-8 py-[1.30rem] bg-[#fff] flex justify-center items-center">
       <div className="max-w-[1140px] w-full mx-auto flex justify-between items-center">
@@ -32,7 +32,53 @@ export default function Header() {
           </div>
         ) : (
           <div>
-            <DropDown />
+            {user?.role === "vet_admin" && (
+              <div>
+                <Link
+                  to="/vets/my-appointment"
+                  className={`${
+                    location?.pathname == "/vets/my-appointment"
+                      ? "bg-primary text-gray-800"
+                      : "text-gray-900"
+                  } group flex w-full items-center rounded-md px-4 py-2 text-sm`}
+                >
+                  Appointment List
+                </Link>
+                <Link
+                  to="/vets/accountsettings"
+                  className={`${
+                    location?.pathname == "/vets/accountsettings"
+                      ? "bg-primary text-gray-800"
+                      : "text-gray-900"
+                  } group flex w-full items-center rounded-md px-4 py-2 text-sm`}
+                >
+                  Account Settings
+                </Link>
+                <Link
+                  to="/vets/MyAvailabilities"
+                  className={`${
+                    location?.pathname == "/vets/MyAvailabilities"
+                      ? "bg-primary text-gray-800"
+                      : "text-gray-900"
+                  } group flex w-full items-center rounded-md px-4 py-2 text-sm`}
+                >
+                  Availability
+                </Link>
+                <Link
+                  to="/vets/calender"
+                  className={`${
+                    location?.pathname == "/vets/calender"
+                      ? "bg-primary text-gray-800"
+                      : "text-gray-900"
+                  } group flex w-full items-center rounded-md px-4 py-2 text-sm`}
+                >
+                  Calendar
+                </Link>
+              </div>
+            )}
+            <div>
+              <DropDown />
+            </div>
           </div>
         )}
         {/* {user?.role === "user" && !user?.completed_medical_report && (
